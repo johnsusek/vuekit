8b061ffd582e0f631149ab30a6d9a4163d4040dd
John Susek <john@johnsolo.net>
2021-09-23 22:17:18 -0500

    Use es build instead of iife

--- templates/xcode/Sources/AppDelegate.swift
+++ templates/xcode/Sources/AppDelegate.swift
@@ -7,8 +7,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_: Notification) {
     let context = JSContext()!
     let vueKit = VueKit(context: context)
-
-    guard let bundleUrl = Bundle.main.url(forResource: "vuekit-bundle.iife", withExtension: "js") else {
+        
+    guard let bundleUrl = Bundle.main.url(forResource: "vuekit-bundle.es", withExtension: "js") else {
       print("! Could not find bundle js at app bundle root")
       NSApp.terminate(self)
       return

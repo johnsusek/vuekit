4509619271e50bd5273e85dd3ce5580a9d49db99
John Susek <john@johnsolo.net>
2021-09-25 15:15:41 -0500

    Use release version of .framework for npm package

--- framework/build.sh
+++ framework/build.sh
@@ -4,5 +4,5 @@ rm -rf ./VueKit.xcodeproj
 /Users/john/Code/my-vuekit-app/node_modules/vuekit-vue/vendor/xcodegen/bin/xcodegen generate
 rm -rf ./VueKit.framework
 xcodebuild -quiet
-cp -R build/Debug/VueKit.framework .
+cp -LR build/Release/VueKit.framework .
 rm -rf build

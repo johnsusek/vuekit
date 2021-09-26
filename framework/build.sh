3af6dc4b4996c9a8d1bbde2f7293652a24c974d1
John Susek <john@johnsolo.net>
2021-09-25 21:57:39 -0500

    Add framework to xcodeproj

--- framework/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf ./VueKit.xcodeproj
-/Users/john/Code/my-vuekit-app/node_modules/vuekit-vue/vendor/xcodegen/bin/xcodegen generate
-rm -rf ./VueKit.framework
-xcodebuild -quiet
-cp -LR build/Debug/VueKit.framework .
-rm -rf build
